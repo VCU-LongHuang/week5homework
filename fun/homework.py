abe4563f6d3edbbcad0be76e7beb074e309d1be9
VCU-LongHuang <78453837+VCU-LongHuang@users.noreply.github.com>
2021-03-07 17:13:29 -0500

    first try

--- fun/homework.py
+++ fun/homework.py
@@ -6,7 +6,7 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-    pass
+    return max (incoming_list)
 
 
 def find_least_number(incoming_list):
@@ -14,7 +14,7 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
-    pass
+    return min (incoming_list)
 
 
 def add_list_numbers(incoming_list):
